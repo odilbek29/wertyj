f0f629688072413401596360ffc6af6072ec7cf6
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-02-25 21:33:13 +0500

    Add files via upload

---
+++ odilbek.js
@@ -0,0 +1,8 @@
+let ism=prompt("ism kiriting");
+let harf=prompt("biror harf kiriting");
+
+if(ism.toLowerCase() .includes(harf.toLowerCase ())){
+    alert(ism +"ismida" +harf+ "harfi mavjut")
+}else{
+    alert(ism +"ismida"  +harf+  "harfi mavjut emas")
+}
